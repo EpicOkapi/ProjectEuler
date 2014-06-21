77f9e35220dc0cdf1a59e79611534b81ff5bc009
Lorenzo <epicokapi@gmail.com>
2014-06-22 01:37:41 +0200

    Solved the first problem.

---
+++ src/net/lvuur/projecteuler/Problem1.java
@@ -0,0 +1,22 @@
+package net.lvuur.projecteuler;
+
+public class Problem1 {
+	/*
+	 * Problem 1: Multiples of 3 and 5
+	 * If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+	 * Find the sum of all the multiples of 3 or 5 below 1000.
+	 */
+	
+	public static void main(String[] args) {
+		int total = 0;
+		int max = 1000;
+		
+		for(int i = 0; i < max; i++){
+			if(i % 3 == 0 || i % 5 == 0){
+				total += i;
+			}
+		}
+		
+		System.out.println(total);
+	}
+}
